b535166c3cfa4e05d82ec4ea0507afb56cf987cd
Vico <kuchpet.vic@gmail.com>
2024-05-06 23:04:08 +0200

    Connexion

--- Connexion.php
+++ Connexion.php
@@ -5,39 +5,21 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="../Barre Navigation et Footer/Pictures/Icon.png" type="image/x-icon">
   <title>Boop Adventure</title>
-  <link rel="stylesheet" href="Login.css">
-  <link rel="stylesheet" href="../Bar and Footer/Footer/Footer.css">
-  <link rel="stylesheet" href="../Bar and Footer/NavBar/NavBar.css">
-  <link rel="stylesheet" href="../Bar and Footer/Footer/Terms.css">
+  <link rel="stylesheet" href="Connexion.css">
 </head>
 
 <body>
-  <div class="cloud1">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud2">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud3">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud4">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud5">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud6">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud7">
-    <img src="Clouds/pngwing.com.png">
-  </div>
-  <div class="cloud8">
-    <img src="Clouds/pngwing.com.png">
-  </div>
+
+  <?php
+  for ($i = 1; $i <= 8; $i++) {
+    echo "
+    <div class=\"cloud$i\">
+      <img src=\"Nuages_IMG/pngwing.com.png\">
+    </div>
+  ";
+  }
+  ?>
 
   <div class="center">
     <h1>Connexion</h1>
