11016dc7f1be3cdd964451e2ab19b3fb8e2099f3
Vico <kuchpet.vic@gmail.com>
2024-05-06 23:39:50 +0200

    Correction

--- Connexion/Connexion.php
+++ Connexion/Connexion.php
@@ -1,5 +1,25 @@
 <?php
 
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=boop_adventure;charset=utf8;', 'root', 'root');
+
+
+if (isset($_POST['Envoyer'])) {
+  if (!empty($_POST['Mail']) and !empty($_POST['Password'])) {
+    $Mail = htmlspecialchars($_POST['Mail']);
+    $Password = ($_POST['Password']);
+
+
+    $Utilisateur = $bdd->prepare('SELECT * FROM clients WHERE Mail = ? AND Passwords = ?');
+    $Utilisateur->execute(array($Mail, $Password));
+    if ($Utilisateur->rowCount() > 0) {
+      $_SESSION['email'] = $Mail;
+      $_SESSION['mdp'] = $Password;
+
+      header('Location: ../Téléchargement/Téléchargement.php');
+    }
+  }
+}
 
 
 ?>
@@ -31,19 +51,19 @@
     <h1>Connexion</h1>
     <form method="POST">
       <div class="txt_field">
-        <input type="email" name="email" required autocomplete="on">
+        <input type="email" name="Mail" required autocomplete="on">
         <span></span>
         <label>Email</label>
       </div>
       <div class="txt_field">
-        <input type="password" name="mdp" required autocomplete="on">
+        <input type="password" name="Password" required autocomplete="on">
         <span></span>
         <label>Mot de passe</label>
       </div>
       <a href="../Mot de Passe Oublié/forgot_password.php">
         <div class="pass">Mot de passe oublié?</div>
       </a>
-      <input type="submit" class='submit' name='envoie' value="Connexion">
+      <input type="submit" class='submit' name='Envoyer' value="Connexion">
       <div class="signup_link">
         T'as pas ton équipement? <a href="Register.php">Incris toi!</a>
       </div>
