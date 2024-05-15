d9ec60ef93335d1f5f3299ca4ba3fa52e9496842
Théo LESAGE <theo.lesage@student.junia.com>
2024-05-15 13:07:53 +0200

    modif Nos Etudes de la page A propos

--- Boutique test/Boutique.php
+++ Boutique test/Boutique.php
@@ -60,24 +60,23 @@
   </div>
 
   <?php
-
-
+  
   $bdd = new PDO('mysql:host=localhost;dbname=boop_adventure;charset=utf8;', 'root', 'root');
 
 
   if (isset($_POST['Envoyer'])) {
     if (!empty($_POST['Mail']) and !empty($_POST['Password'])) {
-      $Mail = htmlspecialchars($_POST['Mail']);
-      $Password = ($_POST['Password']);
+        $Mail = htmlspecialchars($_POST['Mail']);
+        $Password = ($_POST['Password']);
 
 
-      $Utilisateur = $bdd->prepare('SELECT * FROM clients WHERE Mail = ? AND Passwords = ?');
-      $Utilisateur->execute(array($Mail, $Password));
-      if ($Utilisateur->rowCount() > 0) {
-        $_SESSION['Mail'] = $Mail;
-        $_SESSION['Password'] = $Password;
-      } else {
-      }
+        $Utilisateur = $bdd->prepare('SELECT * FROM clients WHERE Mail = ? AND Passwords = ?');
+        $Utilisateur->execute(array($Mail, $Password));
+        if ($Utilisateur->rowCount() > 0) {
+          $_SESSION['Mail'] = $Mail;
+          $_SESSION['Password'] = $Password;
+        } else {
+        }
     }
   }
 
